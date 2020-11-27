1c1c4c937679c3e94fd7132edbcb32545d29ad82
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-11-27 07:03:09 -0300

    WIP: Adding a text field

--- uikit/ImagePicker/ImagePicker/ViewController.swift
+++ uikit/ImagePicker/ImagePicker/ViewController.swift
@@ -45,5 +45,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         picker.dismiss(animated: true, completion: nil)
     }
+
+    // MARK: Text Field
+
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  1
+    ]
 }
 
