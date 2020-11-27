83026e9b50d4513e64b56f7d574e747ece46587a
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-11-27 06:54:28 -0300

    Allow picking an image from the album or camera

--- uikit/ImagePicker/ImagePicker/ViewController.swift
+++ uikit/ImagePicker/ImagePicker/ViewController.swift
@@ -10,21 +10,39 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func pickAnImage(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
         present(pickerController, animated: true, completion: nil)
     }
 
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
+    }
+
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.originalImage] as! UIImage
-        imagePickerView.image = image
+
+        if let image = info[.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
         picker.dismiss(animated: true, completion: nil)
     }
 }
