435b03a6c1cea4ce638436f9e3c2a2547c6e0684
Ataias Pereira Reis <ataiasreis@gmail.com>
2021-01-05 13:44:39 -0300

    Add SwiftUI playground demo

---
+++ playgrounds/SwiftUI.playground/Contents.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import PlaygroundSupport
+
+
+public struct ProgressView: View {
+    public var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 30.0)
+                .foregroundColor(.blue)
+            Text("25%")
+        }
+    }
+}
+
+struct Preview: View {
+    var body: some View {
+        VStack(spacing: 50) {
+            ProgressView()
+            ProgressView()
+                .environment(\.colorScheme, .dark)
+        }
+        .padding(100)
+        .background(Color(UIColor.secondarySystemBackground))
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    Preview()
+        .frame(width: 600, height: 600, alignment: .center)
+)
+
