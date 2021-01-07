40d23cf51681ebf41ddeb5554ed106033c8f5685
Ataias Pereira Reis <ataiasreis@gmail.com>
2021-01-07 09:13:54 -0300

    Add custom combine publisher initial files

--- playgrounds/SwiftUI.playground/Contents.swift
+++ playgrounds/SwiftUI.playground/Contents.swift
@@ -2,31 +2,38 @@ import SwiftUI
 import PlaygroundSupport
 
 
-public struct ProgressView: View {
-    public var body: some View {
-        ZStack {
-            Circle()
-                .stroke(lineWidth: 30.0)
-                .foregroundColor(.blue)
-            Text("25%")
-        }
-    }
-}
+//public struct ProgressView: View {
+//    public var body: some View {
+//        ZStack {
+//            Circle()
+//                .stroke(lineWidth: 30.0)
+//                .foregroundColor(.blue)
+//            Text("25%")
+//        }
+//    }
+//}
+//
+//struct Preview: View {
+//    var body: some View {
+//        VStack(spacing: 50) {
+//            ProgressView()
+//            ProgressView()
+//                .environment(\.colorScheme, .dark)
+//        }
+//        .padding(100)
+//        .background(Color(UIColor.secondarySystemBackground))
+//    }
+//}
+//
+//PlaygroundPage.current.setLiveView(
+//    Preview()
+//        .frame(width: 600, height: 600, alignment: .center)
+//)
+//
 
-struct Preview: View {
-    var body: some View {
-        VStack(spacing: 50) {
-            ProgressView()
-            ProgressView()
-                .environment(\.colorScheme, .dark)
-        }
-        .padding(100)
-        .background(Color(UIColor.secondarySystemBackground))
-    }
-}
 
-PlaygroundPage.current.setLiveView(
-    Preview()
-        .frame(width: 600, height: 600, alignment: .center)
-)
+let url = URL(string: "themoviemanager://authenticate")!
+let components = URLComponents(url: url, resolvingAgainstBaseURL: true)
 
+components?.scheme
+components?.host
---
+++ swiftui/CustomCombinePublisher/CustomCombinePublisher/CustomCombinePublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCombinePublisherApp.swift
+//  CustomCombinePublisher
+//
+//  Created by Ataias Pereira Reis on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomCombinePublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
